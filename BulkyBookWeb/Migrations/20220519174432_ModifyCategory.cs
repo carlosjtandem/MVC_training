56cd9c9e9065d78c0e8d3516d32171ff2201bf50
carlosjtandem <soycarlosjavier14@gmail.com>
2022-05-19 14:30:10 -0500

    modified appsetings

---
+++ BulkyBookWeb/Migrations/20220519174432_ModifyCategory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBookWeb.Migrations
+{
+    public partial class ModifyCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "dateTime",
+                table: "Categories",
+                newName: "CreatedDateTime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedDateTime",
+                table: "Categories",
+                newName: "dateTime");
+        }
+    }
+}
