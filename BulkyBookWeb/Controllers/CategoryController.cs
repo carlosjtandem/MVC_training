c8fe6a10419f4d7220765bbcb25d1fa0820cf4c6
carlosjtandem <soycarlosjavier14@gmail.com>
2022-05-20 17:52:12 -0500

    created CRUD

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -19,11 +19,103 @@ namespace BulkyBookWeb.Controllers
             return View(objCategoryList);
         }
 
+        //************************ CREATe *******
         public IActionResult Create()
         {
-           
-
             return View();
         }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The displayOrder cannot match the name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+                //return RedirectToAction("Index","OtherController");  // If you want to redirect to another controller and method
+            }
+            return View(obj);
+
+        }
+        //************************ EDIT *******
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDB = _db.Categories.Find(id);
+            //var categoryFromDBFirst = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //var categoryFromDBSingle = _db.Categories.SingleOrDefault(c => c.Id == id);
+
+            if (categoryFromDB == null) {
+                return NotFound();
+            }
+
+            return View(categoryFromDB);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The displayOrder cannot match the name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+                //return RedirectToAction("Index","OtherController");  // If you want to redirect to another controller and method
+            }
+            return View(obj);
+        }
+
+        //************************ DELETE *******
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDB = _db.Categories.Find(id);
+            //var categoryFromDBFirst = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //var categoryFromDBSingle = _db.Categories.SingleOrDefault(c => c.Id == id);
+
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDB);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var categoryFromDB = _db.Categories.Find(id);
+            //var categoryFromDBFirst = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //var categoryFromDBSingle = _db.Categories.SingleOrDefault(c => c.Id == id);
+
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(categoryFromDB);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+       
     }
 }
