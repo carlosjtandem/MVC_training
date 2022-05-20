d245e138f3cf85ff3f5bea19902910600115e60e
carlosjtandem <soycarlosjavier14@gmail.com>
2022-05-20 13:03:48 -0500

    added list of categories

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConectionLocal")
     ));
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
