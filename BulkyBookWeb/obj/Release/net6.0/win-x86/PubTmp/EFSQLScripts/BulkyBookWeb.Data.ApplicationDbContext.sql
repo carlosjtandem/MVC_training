63f9abf9836465505a98741265e154ec024e75d3
carlosjtandem <soycarlosjavier14@gmail.com>
2022-05-23 11:50:13 -0500

    ready for deploy on Azure

---
+++ BulkyBookWeb/obj/Release/net6.0/win-x86/PubTmp/EFSQLScripts/BulkyBookWeb.Data.ApplicationDbContext.sql
@@ -0,0 +1,54 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220519173611_AddCategory')
+BEGIN
+    CREATE TABLE [Categories] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [DisplayOrder] int NOT NULL,
+        [dateTime] datetime2 NOT NULL,
+        CONSTRAINT [PK_Categories] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220519173611_AddCategory')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220519173611_AddCategory', N'6.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220519174432_ModifyCategory')
+BEGIN
+    EXEC sp_rename N'[Categories].[dateTime]', N'CreatedDateTime', N'COLUMN';
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220519174432_ModifyCategory')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220519174432_ModifyCategory', N'6.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
